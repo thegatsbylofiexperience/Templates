6b45a0ff14368a7bb8141fec84f14f7fbfd6597a
Chris Coulter <c.coulter@gmail.com>
2011-11-18 12:59:21 +1100

    initial commit

---
+++ template.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+#This is a template file, the basis for all scripts written in python.
+#Commenting is mandatory, as it will make it easier for others to understand your wacky choices another day
+
+
+######IMPORTS######
+
+
+######DIRECTORIES######
+
+
+######FUNCTIONS or CLASSES######
+
+
+##$##MAIN##$##
+
+
+#####OLD or BAD CODE#####
