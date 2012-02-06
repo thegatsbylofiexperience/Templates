aca8d3ad5727da82c0817a901130f8af28ea72dd
Chris Coulter <c.coulter@gmail.com>
2012-02-07 00:27:31 +1100

    mofo

---
+++ template_cpp.cpp
@@ -0,0 +1,26 @@
+/*TEMPLATE FOR C++
+Leave your thoughts about what's what here
+I.e.
+-> Program Ideas
+-> Bugs
+-> Algorithm ideas etc
+
+*/
+
+// Includes 
+#include 
+
+//Constants
+
+
+// Namespace
+
+using namespace std;
+
+// Functions and Classes
+
+// ,.-/ Main \-.,
+int main (int argc, char *argv[]) 
+{
+  return 0;
+}
