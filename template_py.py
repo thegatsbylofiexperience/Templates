0528f6c66d1b2428b67f03b8ceb94a977a9e7e12
Chris Coulter <c.coulter@gmail.com>
2012-02-06 21:42:40 +1100

    more templates to be started

---
+++ template_py.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+#This is a template file, the basis for all scripts written in python.
+#Commenting is mandatory, as it will make it easier for others to understand your wacky choices another day
+
+
+######IMPORTS######
+
+
+######DIRECTORIES######
+
+
+######CONSTANTS######
+
+
+######FUNCTIONS or CLASSES######
+
+
+##$##MAIN##$##
+if __name__ == '__main__':
+
+#####OLD or BAD CODE#####
---
+++ template_py_mysql.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python
+#This is a template file, the basis for all scripts written in python.
+#Commenting is mandatory, as it will make it easier for others to understand your wacky choices another day
+
+
+######IMPORTS######
+import MySQLdb
+
+
+######DIRECTORIES######
+
+
+######CONSTANTS######
+
+
+######FUNCTIONS OR CLASSES######
+
+	
+##$##MAIN##$##
+if __name__ == '__main__':
+	conn = MySQLdb.connect (host = "localhost", user = "ccoulter", passwd = "melatonin");
+	cursor = conn.cursor();
+	#cursor.executemany("")
+	#cursor.execute ("");
+	#row = cursor.fetchall()
+	#for r in row:
+	#	print "%s" % r ;
+	cursor.close ()
+	conn.close ()
+
+#####OLD or BAD CODE##### Check it down here with an explanation of why it is baaad (all quoted out too!)
